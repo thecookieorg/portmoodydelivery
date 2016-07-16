aaaab264b1789b7ed948c34463c16e2510918463
thecookieorg <marko@thecookie.org>
2016-07-16 08:23:33 -0700

    checkout page added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716142410) do
+ActiveRecord::Schema.define(version: 20160716151225) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -50,11 +50,14 @@ ActiveRecord::Schema.define(version: 20160716142410) do
   create_table "line_items", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 1
+    t.integer  "order_id"
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id"
+  add_index "line_items", ["order_id"], name: "index_line_items_on_order_id"
   add_index "line_items", ["product_id"], name: "index_line_items_on_product_id"
 
   create_table "merchants", force: :cascade do |t|
@@ -69,6 +72,12 @@ ActiveRecord::Schema.define(version: 20160716142410) do
 
   add_index "merchants", ["slug"], name: "index_merchants_on_slug", unique: true
   add_index "merchants", ["user_id"], name: "index_merchants_on_user_id"
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "pay_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
